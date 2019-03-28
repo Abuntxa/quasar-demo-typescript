bd7ab88b85872f5512dc5a17fd0ad128698452fd
Jon Abaunza <jabaunza@sisteplant.com>
2019-03-28 17:29:50 +0100

    vue-property-decorators

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,9 +8,13 @@
 </style>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Vue, Component, Prop } from 'vue-property-decorator'
 
-export default Vue.extend({
-  name: 'PageIndex'
-})
+@Component
+export default class IndexPage extends Vue {
+  @Prop(Number) readonly propA!: number
+  @Prop({ default: 'default value' }) readonly propB!: string
+  @Prop([String, Boolean]) readonly propC!: string | boolean
+  name:string= 'PageIndex'
+}
 </script>
